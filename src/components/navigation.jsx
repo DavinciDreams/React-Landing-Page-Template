a9b6681534bac644f32cd473d840446e11d62ba2
Lisa Mega Watts <64185677+DavinciDreams@users.noreply.github.com>
2024-03-21 23:08:34 +0000

    fairytale.chat

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Neverending Stories
+            Fairytale.Chat
           </a>{" "}
         </div>
 
