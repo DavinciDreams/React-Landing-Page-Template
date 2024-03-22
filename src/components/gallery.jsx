e5b57983dec4668758f3d10181f737a3295ed116
Lisa Mega Watts <64185677+DavinciDreams@users.noreply.github.com>
2024-03-22 01:26:11 +0000

    gallery description

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -8,8 +8,7 @@ export const Gallery = (props) => {
         <div className="section-title">
           <h2>Gallery</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Unleash Your Creativity with the Power of AI Storytelling
           </p>
         </div>
         <div className="row">
