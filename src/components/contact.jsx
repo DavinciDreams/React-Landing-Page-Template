d1cf61a2fefc71ac95a1e669f8511638315b32c1
Lisa Mega Watts <64185677+DavinciDreams@users.noreply.github.com>
2024-03-29 07:29:14 +0000

    remove socials

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -127,26 +127,7 @@ export const Contact = (props) => {
             <div className="row">
               <div className="social">
                 <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.discord : "/"}>
-                      <i className="fa fa-discord"></i>
-                    </a>
-                  </li>
+              
                 </ul>
               </div>
             </div>
